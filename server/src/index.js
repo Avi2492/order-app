fd8b0bc11127308072d367a2b345ab4cfa08fc51
Avinash Pandey <avinash.12009084@lpu.in>
2024-03-11 16:58:53 +0530

    Cookie and App.js Work

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,20 @@
 import * as dotenv from "dotenv";
 import databaseConnection from "./db/databaseConnection.js";
+import { app } from "./app.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-databaseConnection();
+databaseConnection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection error: ", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
